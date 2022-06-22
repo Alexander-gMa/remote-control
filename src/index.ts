af6b800a0e0e6b0db26a460bb630a71115a2d4df
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-06-22 16:02:58 +0300

    feat: create WebSocketServer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 // import Jimp from 'jimp';
 import { httpServer } from './http_server/index';
 // import robot from 'robotjs';
-// import { WebSocketServer } from 'ws';
+import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 3000;
+const WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wsServer = new WebSocketServer({ port: WS_PORT });
+wsServer.on("listening", () => {
+    console.log(`Start WebSocketServer on the ${WS_PORT} port!`)
+})
