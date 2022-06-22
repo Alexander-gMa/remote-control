a13593716975258a3f9063c92694a6f06ba025ca
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-06-22 16:11:45 +0300

    feat: catch favicon error

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -13,6 +13,11 @@ export const httpServer = http.createServer((req, res) => {
         const readStream = fs.createReadStream(file_path);
         res.writeHead(200);
         readStream.pipe(res);
+        readStream.on('error', (err) => {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+        }
+        )
     } catch (error) {
         res.writeHead(404);
         res.end(JSON.stringify(error));
