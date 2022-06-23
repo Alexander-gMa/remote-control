98349218456663cbeb24aeb7ef446891b5868874
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-06-23 16:05:12 +0300

    feat: add function for drawing figure

--- src/commands/move.mouse.ts
+++ src/commands/move.mouse.ts
@@ -1,4 +1,6 @@
 import robot, { mouseToggle } from 'robotjs';
+import { draw_angle_figure } from './figure/angleFigure';
+import { draw_circle } from './figure/circle';
 
 export const move_mouse = (method: string, x: number, y: number, value: Array<string>) => {
     switch (method) {
@@ -15,30 +17,11 @@ export const move_mouse = (method: string, x: number, y: number, value: Array<st
             robot.moveMouse(x + +value[0], y);
             break;
         case 'draw_circle':
-            mouseToggle('down');
-            for (let i = 0; i <= Math.PI * 2; i += 0.06) {
-                const newX = x - +value[0] + (+value[0] * Math.cos(i));
-                const newY = y + (+value[0] * Math.sin(i));
-                robot.moveMouse(newX, newY);
-            }
-            mouseToggle('up');
+            draw_circle(x, y, value);
             break;
         case 'draw_rectangle':
         case 'draw_square':
-            mouseToggle('down');
-            for (let i = 0; i <= (+value[0]); i += 4) {
-                robot.moveMouse(x + i, y);
-            }
-            for (let i = 0; i <= (+value[1] || +value[0]); i += 4) {
-                robot.moveMouse(x + +value[0], y + i);
-            }
-            for (let i = 0; i <= +value[0]; i += 4) {
-                robot.moveMouse(x + +value[0] - i, y + (+value[1] || +value[0]));
-            }
-            for (let i = 0; i <= (+value[1] || +value[0]); i += 4) {
-                robot.moveMouse(x, y + (+value[1] || +value[0]) - i);
-            }
-            mouseToggle('up');
+            draw_angle_figure(x, y, value);
             break;
         default:
             throw new Error();
